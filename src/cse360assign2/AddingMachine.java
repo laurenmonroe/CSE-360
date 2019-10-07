8274338eb1d6df3a15992d7abd8025c06d401e0b
laurenmonroe <lmonroe2@asu.edu>
2019-10-07 13:28:58 -0700

    initial version

---
+++ src/cse360assign2/AddingMachine.java
@@ -0,0 +1,67 @@
+/**
+ * Name: Lauren Monroe
+ * Class: CSE 360 Monday 9:40
+ * ID: 1215051617
+ * Assignment: #1
+ * Description: This file is the AddingMachine class
+*/
+
+package cse360assign2;
+
+/**
+ * The AddingMachine class creates a AddingMachine object which has an
+ * integer instance variable. The class also contains methods
+ * to add and subtract, as well as return the total
+*/
+public class AddingMachine {
+
+	private int total;
+
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+
+	/**
+		 * Returns an int that is the total
+		 *
+		 * @return int total
+		*/
+	public int getTotal () {
+		return 0;
+	}
+
+	/**
+		 * Adds a number to the total
+		 *
+		 * @param int value to be added to the total
+		*/
+	public void add (int value) {
+
+	}
+
+	/**
+		 * Subtracts a number from the total
+		 *
+		 * @param value to be subtracted
+		*/
+	public void subtract (int value) {
+
+	}
+
+	/**
+		 * Returns an String representation of the total
+		 *
+		 * @return String formats the answer and returns it as a String
+		*/
+	public String toString () {
+		return "";
+	}
+
+	/**
+		 * Clears the total
+		 *
+		*/
+	public void clear() {
+
+	}
+}
